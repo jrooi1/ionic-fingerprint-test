e61cfea2b1d5a12b4b6a5b0f7971b7d7e04b67e4
josh <josh.ronald.ooi@gmail.com>
2020-12-03 11:37:56 +0800

    mad minor changes

--- src/app/dashboard/dashboard.page.ts
+++ src/app/dashboard/dashboard.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FingerprintAIO } from '@ionic-native/fingerprint-aio/ngx';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardPage implements OnInit {
 
-  constructor() { }
+    constructor(
+        private fingerprint: FingerprintAIO
+    ) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
+
+    public login(): void {
+        this.fingerprint.loadBiometricSecret({
+
+        })
+    }
 
 }
